ade49b0c728e35876c36fff6e3fc6589d15997d7
Tyler <yippieskippie24@gmail.com>
2016-07-13 18:26:10 -0500

    Cleaned up the main menu
    Changed the main menu to use an array instead of static entries.  Also added the option to specify user in custom connection. cleaned up other minor issues.

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -8,7 +8,7 @@
 #      to internal servers through a SSH jumpbox.         #
 #                                                         #
 #              Written by:  Tyler M Johnson               #
-#         https://github.com/yippieskippie24              #
+#           https://github.com/yippieskippie24            #
 #                                                         #
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
@@ -16,15 +16,21 @@
 # This script is written with the assumption that it      #
 # resides in the home folder of the user that runs it.    #
 # You will need to set the names of the variables         #
-# below to the FQDNs of the servers you want to be        #
-# accessible in this script.  You can also change the     #
-# ports as needed. You can edit the server descriptions   #
-# as needed below.                                        #
+# below to the FQDNs and descriptions of the servers you  #
+# want to be accessible in this script. You can also      #
+# change the ports as needed.                             #
+#                                                         #
+# If you want fewer server options listed just remove the #
+# content of the variable. For instance:                  #
+#                  server10=                              #
+#                  server10_desc=                         #
+#                  server10_port=                         #
+#                                                         #
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
 #editable Variables
 server1="server1-hostname"
-server1_desc="server1-descritpion"
+server1_desc="server1-description"
 server1_port="22"
 
 server2="server2-hostname"
@@ -51,6 +57,18 @@ server7="server7-hostname"
 server7_desc="server7-description"
 server7_port="22"
 
+server8="server8-hostname"
+server8_desc="server8-description"
+server8_port="22"
+
+server9="server9-hostname"
+server9_desc="server9-description"
+server9_port="22"
+
+server10=
+server10_desc=
+server10_port=
+
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 #                                                                    #
 #                 Do not edit below this section                     #
@@ -65,21 +83,38 @@ serverSELECTION=""
 portSELECTION=""
 
 
+#Servers + descriptions Array
+servers=(\
+$server1 $server1_desc \
+$server2 $server2_desc \
+$server3 $server3_desc \
+$server4 $server4_desc \
+$server5 $server5_desc \
+$server6 $server6_desc \
+$server7 $server7_desc \
+$server8 $server8_desc \
+$server9 $server9_desc \
+$server10 $server10_desc \
+)
+
+
+#Default menu Array
+defaultmenu=(\
+Custom "Connect to another server" \
+Advanced "Advanced Menu" \
+Exit "Exit this script" \
+)
+
+#combine Servers + descriptions Array with Default menu Array
+mainMenuARRAY=("${servers[@]}" "${defaultmenu[@]}")
+
+
 #MainMenu
 
 function mainMenu() {
-	serverSELECTION=$(whiptail --title "SSH Connect" --menu "Choose Server to connect to:" 20 65 10 \
-		$server1 $server1_desc \
-		$server2 $server2_desc \
-		$server3 $server3_desc \
-		$server4 $server4_desc \
-		$server5 $server5_desc \
-		$server6 $server6_desc \
-		$server7 $server7_desc \
-		"Custom" "|  Connect to another server" \
-		"Advanced" "|  Advanced Menu" \
-		"Exit" "| Exit this script" 3>&1 1>&2 2>&3)
-	
+	serverSELECTION=$(whiptail --title "SSH Connect" --nocancel --menu "Choose Server to connect to:" 30 65 20 \
+		"${mainMenuARRAY[@]}" 3>&1 1>&2 2>&3)
+
 			SSH_port_picker
 
 
@@ -114,14 +149,15 @@ function SSH_port_picker() {
 
 function customServer() {
 			customSERVER=$(whiptail --inputbox "What is the FQDN of the server you want to connect to?" 8 78 --title "Custom server" 3>&1 1>&2 2>&3)
-			portSELECTION=$(whiptail --inputbox "What port do you want to connect on?" 8 78 22--title "Select Port" 3>&1 1>&2 2>&3)
+			portSELECTION=$(whiptail --inputbox "What port do you want to connect on?" 8 78 22 --title "Select Port" 3>&1 1>&2 2>&3)
+			userSELECTION=$(whiptail --inputbox "What user do you want to use to connect?" 8 78 $USER --title "Select User" 3>&1 1>&2 2>&3)
 		if [ "$portSELECTION" = "22" ]; then
 			echo connecting to $customSERVER...
-			ssh $customSERVER
+			ssh $customSERVER -l $userSELECTION
 			~/ssh_connect.sh
 		else
 			echo connecting to $customSERVER on port $portSELECTION...
-			ssh $customSERVER -p $portSELECTION
+			ssh $customSERVER -p $portSELECTION -l $userSELECTION
 			~/ssh_connect.sh
 		fi
 }
