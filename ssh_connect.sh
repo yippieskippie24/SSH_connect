436a59c2fa73558ebc971ac36b60f47425eb3577
Tyler <yippieskippie24@gmail.com>
2016-07-12 19:15:14 -0500

    Changed script to use variables for servers
    Changed script to use variables for server connections.  This allows the script to be much more flexible with the number of connections a user wants to save in this script.

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -20,100 +20,80 @@
 # descriptions as needed below.                           #
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
 
-#Variables
+#editable Variables
 server1="server1-hostname"
+server1_desc="server1-descritpion"
+
 server2="server2-hostname"
+server2_desc="server2-description"
+
 server3="server3-hostname"
+server3_desc="server3-description"
+
 server4="server4-hostname"
+server4_desc="server4-description"
+
 server5="server5-hostname"
+server5_desc="server5-description"
+
 server6="server6-hostname"
+server6_desc="server6-description"
+
 server7="server7-hostname"
+server7_desc="server7-description"
+
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#                                                                    #
+#                 Do not edit below this section                     #
+#                                                                    #
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+
+#static variables
+
 customServer=""
 serverSELECTION=""
 
 
 #MainMenu
-					#edit the server description below as needed to match the name of the servers
 
 function mainMenu() {
 	serverSELECTION=$(whiptail --title "SSH Connect" --menu "Choose Server to connect to:" 20 65 10 \
-		$server1 "|  server1 description" \
-		$server2 "|  server2 description" \
-		$server3 "|  server3 description" \
-		$server4 "|  server4 description" \
-		$server5 "|  server5 description" \
-		$server6 "|  server6 description" \
-		$server7 "|  server7 description" \
+		$server1 $server1_desc \
+		$server2 $server2_desc \
+		$server3 $server3_desc \
+		$server4 $server4_desc \
+		$server5 $server5_desc \
+		$server6 $server6_desc \
+		$server7 $server7_desc \
 		"Custom" "|  Connect to another server" \
+		"Advanced" "|  Advanced Menu" \
 		"Exit" "| Exit this script" 3>&1 1>&2 2>&3)
 
-#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*##*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
-		
-	case $serverSELECTION in
-		$server1)
-			clear
-			echo connecting to $server1...
-			ssh $server1
-			~/ssh_connect.sh
-		;;
-		
-		$server2)
-			clear
-			echo connecting to $server2...
-			ssh $server2
-			~/ssh_connect.sh
-		;;
-
-		$server3)
-			clear
-			echo connecting to $server3...
-			ssh $server3
-			~/ssh_connect.sh
-		;;
-
-		$server4)
-			clear
-			echo connecting to $server4...
-			ssh $server4
-			~/ssh_connect.sh
-		;;
-
-		$server5)
-			clear
-			echo connecting to $server5...
-			ssh $server5
-			~/ssh_connect.sh
-		;;
-
-		$server6)
-			clear
-			echo connecting to $server6...
-			ssh $server6
-			~/ssh_connect.sh
-		;;
-
-		$server7)
-			clear
-			echo connecting to $server7...
-			ssh $server7
-			~/ssh_connect.sh
-		;;
-
-		Custom)
+	if [ "$serverSELECTION" = "Custom" ]; then
 			customServer=$(whiptail --inputbox "What is the FQDN of the server you want to connect to?" 8 78 --title "Custom server" 3>&1 1>&2 2>&3)
 			clear
 			echo connecting to $customServer...
 			ssh $customServer
 			~/ssh_connect.sh
-		;;
-		
-		Exit)
-		clear
-		echo exiting SSH Connect
-		exit
-			
-	esac
 
+		elif [ "$serverSELECTION" = "Advanced" ]; then
+			echo advanced Menu
+			sleep 10
+			~/ssh_connect.sh
+
+		elif [ "$serverSELECTION" = "Exit" ]; then
+			clear
+			echo exiting SSH Connect
+			exit
+
+		else
+			echo connecting to $serverSELECTION...
+			ssh $serverSELECTION
+			~/ssh_connect.sh
+
+	fi
 }
 
 #This section actually calls the mainMenu function
